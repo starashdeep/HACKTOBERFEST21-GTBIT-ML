05d69fe7f9bd2e70063861ce6d376d0d53b54afb
KanishkKhurana <74613246+KanishkKhurana@users.noreply.github.com>
2021-10-05 20:47:29 +0530

    added data visualisation programs

---
+++ Intermediate/Data Visualisation/basic_matplotlib.py
@@ -0,0 +1,23 @@
+# importing the required module
+
+	
+# x axis values
+
+# corresponding y axis values
+	
+# plotting the points
+
+	
+# naming the x axis
+
+# naming the y axis
+
+	
+# giving a title to my graph
+
+	
+# function to show the plot
+
+
+#print your name , college name , year of graduation at the end of the program
+
---
+++ Intermediate/Data Visualisation/seaborn_basics.py
@@ -0,0 +1,15 @@
+
+
+# importing packages
+
+
+# loading dataset
+data = sns.load_dataset("Name_of_your_dataset")
+
+# draw lineplot
+
+
+
+#print your name , college name , year of graduation at the end of the program
+
+
