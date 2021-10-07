42040d96df8c7556143cc72593a599bc8afce319
Manas Bhilare <69368910+Manas146@users.noreply.github.com>
2021-10-07 23:16:29 +0530

    Update basic_matplotlib.py
    Thanks for such a wonderful opportunity!

--- Intermediate/Data Visualisation/basic_matplotlib.py
+++ Intermediate/Data Visualisation/basic_matplotlib.py
@@ -1,23 +1,24 @@
 # importing the required module
 
-	
+import matplotlib.pyplot as plt 
 # x axis values
-
+x = [1,2,3] 
 # corresponding y axis values
-	
+y = [2,4,1] 
 # plotting the points
-
+plt.plot(x, y)
 	
 # naming the x axis
-
+plt.xlabel('x - axis') 
 # naming the y axis
-
+plt.ylabel('y - axis')
 	
 # giving a title to my graph
-
+plt.title('basic_matploitlib')
 	
 # function to show the plot
-
+plt.show() 
 
 #print your name , college name , year of graduation at the end of the program
 
+print("Manas Bhilare", "Dr. D.Y. Patil Institue of Technolgy", "Year of Graduation 2023")
