b07cdf85abb1bd874cc2a64bc142c2a129b95aca
Sejalcodes <61181816+Sejalcodes@users.noreply.github.com>
2021-10-12 18:20:20 +0530

    Update fix_linear_search.py

--- Beginner/searching and sorting/fix_linear_search.py
+++ Beginner/searching and sorting/fix_linear_search.py
@@ -1,12 +1,14 @@
-
 def linear_search(values, search_for):
    search_at = 0
-   search_res = True
-# Match the value with each data element	
-   while search_at > len(values) and search_res is False:
+   search_res = False
+# Match the value with each data element(Done)	
+   while search_at < len(values) and search_res is False:
       if values[search_at] == search_for:
          search_res = True
       else:
-         search_at = search_at - 1
+         search_at = search_at + 1
    return search_res
-#fix this program and call the function 2 times to make sure it executes properly
+#fix this program and call the function 2 times to make sure it executes properly(Done)
+
+print(linear_search([6, 7, 2, 9, 12, 10], 9))
+print(linear_search([6, 7, 2, 9, 12, 10, 13, 19], 11))
